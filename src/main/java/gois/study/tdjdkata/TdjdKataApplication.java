51b0153661cbeea732c22bc306604f3f4c3d7451
Gustavo Gois <gustavo.gois@gmail.com>
2019-09-11 18:07:51 +0100

    Initial commit

---
+++ src/main/java/gois/study/tdjdkata/TdjdKataApplication.java
@@ -0,0 +1,13 @@
+package gois.study.tdjdkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdjdKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdjdKataApplication.class, args);
+    }
+
+}
